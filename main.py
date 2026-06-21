4d8d0347f85831c53dc680a44a63a2b87567031f
0xNullVOID <0xNullVOID@proton.me>
2026-06-21 12:02:31 +0200

    fixed proper naming conventions

--- main.py
+++ main.py
@@ -2,26 +2,26 @@
 import time
 import pygetwindow as gw
 
-TARGET_WINDOW_TITLE = "Task Manager" # TODO make generic, customizable, configurable, with keybinds and manual entry in a config file
-targetWindowFocused = False
+target_window_title = "Task Manager" # TODO make generic, customizable, configurable, with keybinds and manual entry in a config file
+target_window_focused = False
 
-def getActiveWindow():
+def get_active_window():
     active_window = gw.getActiveWindow()
     print(f'Active Window: {active_window}')
     time.sleep(0.5) # slow down script, prevent infinite checking
     return active_window
 
 def run():
-    global targetWindowFocused
+    global target_window_focused
     while True:
-        window = getActiveWindow()
-        if window.title == TARGET_WINDOW_TITLE:
-            targetWindowFocused = True
+        window = get_active_window()
+        if window.title == target_window_title:
+            target_window_focused = True
             print(f'Target Window in FOCUS')
         else:
-            if targetWindowFocused:
+            if target_window_focused:
                 print(f'Lost FOCUS')
-            targetWindowFocused = False
+            target_window_focused = False
 
 
 if __name__ == '__main__':
