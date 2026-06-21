92528518e87ce0b500efb79a802833506f3c00f0
0xNullVOID <0xNullVOID@proton.me>
2026-06-21 12:02:31 +0200

    Detect targeted window using window title and checks if its in focus or not

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ import time
 import pygetwindow as gw
 
 TARGET_WINDOW_TITLE = "Task Manager" # TODO make generic, customizable, configurable, with keybinds and manual entry in a config file
+targetWindowFocused = False
 
 def getActiveWindow():
     active_window = gw.getActiveWindow()
     print(f'Active Window: {active_window}')
     time.sleep(0.5) # slow down script, prevent infinite checking
+    return active_window
 
 def run():
+    global targetWindowFocused
     while True:
-        getActiveWindow()
+        window = getActiveWindow()
+        if window.title == TARGET_WINDOW_TITLE:
+            targetWindowFocused = True
+            print(f'Target Window in FOCUS')
+        else:
+            if targetWindowFocused:
+                print(f'Lost FOCUS')
+            targetWindowFocused = False
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     run()
