b47542a3e0b3a6216760bc09504bf894ce3f59b9
0xNullVOID <0xNullVOID@proton.me>
2026-06-21 12:02:31 +0200

    Detect active window

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+import pygetwindow as gw
 
+TARGET_WINDOW_TITLE = "Task Manager" # TODO make generic, customizable, configurable, with keybinds and manual entry in a config file
+
+def getActiveWindow():
+    active_window = gw.getActiveWindow()
+    print(f'Active Window: {active_window}')
+    time.sleep(0.5) # slow down script, prevent infinite checking
+
+def run():
+    while True:
+        getActiveWindow()
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("Hello World")
+    run()
