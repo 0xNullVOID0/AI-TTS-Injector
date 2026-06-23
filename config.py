2696b13d9e16e8286d7b2bca6f99da363cb34e65
0xNullVOID <0xNullVOID@proton.me>
2026-06-23 17:42:08 +0200

    turned config into a class as well, a singleton for easy global access to all settings and other variables

--- config.py
+++ config.py
@@ -3,51 +3,59 @@ import os
 
 CONFIG_FILE = "config.json"
 LOCAL_CONFIG_FILE = "config.local.json"
-data = None
 
-# TODO turn into class? easy global target_window_title and other settings available?
 
-def load_json(config=LOCAL_CONFIG_FILE):
-    global data
-    if os.path.exists(config):
-        print(f"Loading config from {config}")
-        try:
-            with open(config, 'r') as f:
-                data = json.load(f)
-                print(data)
+class _Config:
+    def __init__(self, filename):
+        self.json = None
+        self.load(filename)
+        self.target_window_title = self.json["TARGET_WINDOW_TITLE"]
+        self.voice_map = self.json['VOICE_MAP'] # Map character names to the desired Kokoro voice codes
 
-                if config == CONFIG_FILE:
-                    print(f"Creating local {LOCAL_CONFIG_FILE} file automatically...")
-                    with open(LOCAL_CONFIG_FILE, 'w') as f:
-                        json.dump(data, f, indent=4)
+        print(f'Config object init: {self.json}')
+        print(f"voice map: {self.voice_map}")
 
-                return data
-        except Exception:
-            pass
-    else:
-        print(f"CONFIG not found")
-        return None
+    def load_json(self, filename):
+        if os.path.exists(filename):
+            print(f"Loading config from {filename}")
+            try:
+                with open(filename, 'r') as f:
+                    self.json = json.load(f)
 
-def load_config():
-    if load_json(LOCAL_CONFIG_FILE):
-        return data
-    elif load_json(CONFIG_FILE):
-        return data
-    else:
-        print("ERROR: config not found")
-        return None
+                    if filename == CONFIG_FILE:
+                        print(f"Creating local {LOCAL_CONFIG_FILE} file automatically...")
+                        with open(LOCAL_CONFIG_FILE, 'w') as f:
+                            json.dump(self.json, f, indent=4)
 
-# turns key into proper json format and style
-def cleanup_key(key):
-    return str(key).replace(" ", "_").upper()
+                    return True
+            except Exception as e:
+                print(f"Failed to load config from {filename}, error: {e}")
+                pass
+        else:
+            print(f"CONFIG not found")
+            return None
 
-def update_config(key, value):
-    global data
-    if not data:
-        data = load_json()
 
-    print(f"Updating {key} to {value}")
-    data[key] = value
-    with open(LOCAL_CONFIG_FILE, 'w') as f:
-        json.dump(data, f, indent=4)
-    print(f"Saved {key} to {LOCAL_CONFIG_FILE}")
+    def load(self, filename):
+        loaded = False
+        if os.path.exists(LOCAL_CONFIG_FILE):
+            loaded = self.load_json(LOCAL_CONFIG_FILE)
+        if not loaded:
+            loaded = self.load_json(CONFIG_FILE)
+        return loaded
+
+    def update(self, key, value):
+        print(f"Updating {key} to {value}")
+        self.json[key] = value
+        with open(LOCAL_CONFIG_FILE, 'w') as f: # TODO make local config file variable dynamic here
+            json.dump(self.json[key], f, indent=4)
+        print(f"Saved {key} to {LOCAL_CONFIG_FILE}")
+
+
+    # turns key into proper json format and style
+    @staticmethod
+    def cleanup_key(key):
+        return str(key).replace(" ", "_").upper()
+
+
+config = _Config(CONFIG_FILE)
