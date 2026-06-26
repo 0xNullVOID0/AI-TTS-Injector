c206e4a790e5819d287d8152fce4dace1af78f61
0xNullVOID <0xNullVOID@proton.me>
2026-06-26 17:31:50 +0200

    created global APIClient class for cleaner, more readable and reusable code

--- utils.py
+++ utils.py
@@ -42,3 +42,46 @@ def json_serial(obj):
 
 data = '{"name": "Joker", "active": true}'
 print(is_json(data))
+
+# TODO move to own file?
+# Global APIClient class for cleaner, more readable and reusable code
+class APIClient:
+    def __init__(self, BASE_URL, HEADERS, client=None):  # TODO set user name fix hardcode
+        self.BASE_URL = BASE_URL
+        self.HEADERS = HEADERS
+        self.client = client
+
+
+    def api(self, route="/", payload=None, req_type=None):
+        url = f"{self.BASE_URL}{route}"  # TODO improve
+        print(f"Calling: {url}")
+
+        response = None
+
+        if payload:
+            if is_json(payload):
+                response = requests.post(url, json=payload, headers=self.HEADERS, timeout=120)
+            else:
+                # response = requests.post(url, headers=self.HEADERS, timeout=120)
+                print(f"ERROR: payload {payload} failed, NOT (proper?) JSON") # TODO redundant?
+                response = Exception("ERROR: payload {payload} failed, NOT (proper?) JSON")
+            # except Exception as e:
+            #     print(f"ERROR: request {url} failed")
+
+            # if is_json(payload):
+            #     response = requests.post(url, json=payload, headers=self.HEADERS, timeout=120)
+            # else:
+            #     response = requests.post(url, payload=payload, headers=self.HEADERS, timeout=120)
+        elif req_type == "GET":
+            response = requests.get(url, headers=self.HEADERS, timeout=120)
+        else:
+            response = requests.post(url, headers=self.HEADERS, timeout=120)
+
+        if response.status_code == 200:
+            print(response)
+        else:
+            print(f"ERROR: request {url} failed")
+            # Exception("ERROR: payload {payload} failed, NOT (proper?) JSON")
+
+        return response
+
